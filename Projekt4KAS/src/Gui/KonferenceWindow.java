141fce26c68f7e2b4f636a6521534eba9a4b23e7
tuxzo <tuxzo@Spil-PC>
2016-04-30 20:54:49 +0200

    Signed-off-by: tuxzo <tuxzo@Spil-PC>

--- Projekt4KAS/src/Gui/KonferenceWindow.java
+++ Projekt4KAS/src/Gui/KonferenceWindow.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
@@ -11,6 +12,7 @@ import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -25,264 +27,290 @@ import java.util.Optional;
 import Model.*;
 import Service.*;
 
-public class KonferenceWindow extends Stage {
-    private Miljøkonference konference;
+public class KonferenceWindow extends Stage
+{
+	private Miljøkonference konference;
 
-    public KonferenceWindow(String title, Miljøkonference konference) {
-        this.initStyle(StageStyle.UTILITY);
-        this.initModality(Modality.APPLICATION_MODAL);
-        this.setResizable(false);
+	public KonferenceWindow(String title, Miljøkonference konference)
+	{
+		this.initStyle(StageStyle.UTILITY);
+		this.initModality(Modality.APPLICATION_MODAL);
+		this.setResizable(false);
 
-        this.konference = konference;
+		this.konference = konference;
 
-        this.setTitle(title);
-        GridPane pane = new GridPane();
-        this.initContent(pane);
+		this.setTitle(title);
+		GridPane pane = new GridPane();
+		this.initContent(pane);
 
-        Scene scene = new Scene(pane);
-        this.setScene(scene);
-    }
+		Scene scene = new Scene(pane);
+		this.setScene(scene);
+	}
 
-    public KonferenceWindow(String title) {
-        this(title, null);
-    }
+	public KonferenceWindow(String title)
+	{
+		this(title, null);
+	}
 
-    // -------------------------------------------------------------------------
-    private TextField[] txfInput;
-    private Label[] lblInput;
-    private String[] lblNames;
-    private Label lblError;
-    private ListView<Prisgruppe> lvwPrisgrupper;
+	// -------------------------------------------------------------------------
+	private TextField[] txfInput;
+	private Label[] lblInput;
+	private String[] lblNames;
+	private Label lblError;
+	private ListView<Prisgruppe> lvwPrisgrupper;
 
-    //--------------------------------------------------------------------------
+	// --------------------------------------------------------------------------
 
+	private void initContent(GridPane pane)
+	{
+		pane.setPadding(new Insets(10));
+		pane.setHgap(10);
+		pane.setVgap(10);
+		pane.setGridLinesVisible(false);
 
-    private void initContent(GridPane pane) {
-        pane.setPadding(new Insets(10));
-        pane.setHgap(10);
-        pane.setVgap(10);
-        pane.setGridLinesVisible(false);
-
-        lblNames = new String[]{"Titel:", "Tema:", "Start Dato:", "Slut Dato:",  "Vej:", "Nr:", "Etage:"
-				, "Postnr:", "By", "Land:"};
+		lblNames = new String[]
+		{"Titel:", "Tema:", "Start Dato:", "Slut Dato:", "Vej:", "Nr:", "Etage:", "Postnr:", "By", "Land:"};
 		txfInput = new TextField[lblNames.length];
 		lblInput = new Label[lblNames.length];
-		
+
 		final int MAX_ROWS = 6;
-		
+
 		for (int i = 0; i < lblNames.length; i++)
 		{
 			if (i < MAX_ROWS)
 			{
 				lblInput[i] = new Label(lblNames[i]);
 				pane.add(lblInput[i], 0, i);
-				
+
 				txfInput[i] = new TextField();
 				pane.add(txfInput[i], 1, i);
-			}
-			else
+			} else
 			{
 				lblInput[i] = new Label(lblNames[i]);
-				pane.add(lblInput[i], 2, i-MAX_ROWS);
-				
+				pane.add(lblInput[i], 2, i - MAX_ROWS);
+
 				txfInput[i] = new TextField();
-				pane.add(txfInput[i], 3, i-MAX_ROWS);
-				
+				pane.add(txfInput[i], 3, i - MAX_ROWS);
+
 			}
-		
+
 		}
-		
+
 		Button btnOK = new Button("OK");
-		pane.add(btnOK, 0, MAX_ROWS+1);
+		pane.add(btnOK, 0, MAX_ROWS + 1);
 		btnOK.setOnAction(event -> this.okAction());
-		
+
 		Button btnCancel = new Button("Cancel");
-		pane.add(btnCancel, 1, MAX_ROWS+1);
+		pane.add(btnCancel, 1, MAX_ROWS + 1);
 		btnCancel.setOnAction(event -> this.cancelAction());
-		
-		PrisgruppeWindow prisgrpw = new PrisgruppeWindow("Opret Prisgruppe", konference);
-		
+
+
 		lvwPrisgrupper = new ListView<>();
 		lvwPrisgrupper.setMinSize(200, 160);
 		lvwPrisgrupper.setMaxSize(200, 160);
 		try
 		{
-				lvwPrisgrupper.getItems().setAll(konference.getPrisgrupper());
+			lvwPrisgrupper.getItems().setAll(konference.getPrisgrupper());
 
-			
 		} catch (NullPointerException ex)
 		{
 			// do nothing
 		}
 
-
 		pane.add(lvwPrisgrupper, 4, 1, 2, 5);
-		
+
 		Label lblPrisgrupper = new Label("Prisgrupper");
 		pane.add(lblPrisgrupper, 4, 0);
-		
+
 		Button btnTilføj = new Button("Tilføj");
 		pane.add(btnTilføj, 4, 7);
 		btnTilføj.setOnAction(event -> this.createPrisgruppeAction());
-		
-		Button btnFjern = new Button ("Fjern");
+
+		Button btnFjern = new Button("Fjern");
 		pane.add(btnFjern, 5, 7);
 		btnFjern.setOnAction(event -> this.deletePrisgruppeAction());
 
-        lblError = new Label();
-        pane.add(lblError, 0, MAX_ROWS+2, 2, 1);
-        lblError.setStyle("-fx-text-fill: red");
+		lblError = new Label();
+		pane.add(lblError, 0, MAX_ROWS + 2, 2, 1);
+		lblError.setStyle("-fx-text-fill: red");
 
-        this.initControls();
-    }
+		this.initControls();
+	}
 
-    private void initControls() {
-        if (konference != null) {
-        	
-        	txfInput[0].setText(konference.getTitel());
-        	txfInput[1].setText(konference.getTema());
-        	txfInput[2].setText(konference.getStartDato().toString());
-        	txfInput[3].setText(konference.getSlutDato().toString());
-        	txfInput[4].setText(konference.getAdresse().getVej());
-        	txfInput[5].setText(""+konference.getAdresse().getNr());
-        	txfInput[6].setText(konference.getAdresse().getEtage());
-        	txfInput[7].setText(""+konference.getAdresse().getPostNr());
-        	txfInput[8].setText(konference.getAdresse().getBy());
-        	txfInput[9].setText(konference.getAdresse().getLand());      	
-        	
-
-        } else {
-        	
-        	txfInput[0].clear();
-        	txfInput[1].clear();
-        	txfInput[2].clear();
-        	txfInput[3].clear();
-        	txfInput[4].clear();
-        	txfInput[5].clear();
-        	txfInput[6].clear();
-        	txfInput[7].clear();
-        	txfInput[8].clear();
-        	txfInput[9].clear();
-        }
-    }
-
-    // -------------------------------------------------------------------------
-
-    private void cancelAction() {
-        this.hide();
-    }
-
-    private void okAction() {
-    	
-    	String titel = txfInput[0].getText().trim();
-    	String tema = txfInput[1].getText().trim();
-    	
-    	LocalDate startDato = LocalDate.MIN;
-    	try
+	private void initControls()
+	{
+		if (konference != null)
 		{
-        	startDato = LocalDate.parse(txfInput[2].getText().trim());
+
+			txfInput[0].setText(konference.getTitel());
+			txfInput[1].setText(konference.getTema());
+			txfInput[2].setText(konference.getStartDato().toString());
+			txfInput[3].setText(konference.getSlutDato().toString());
+			txfInput[4].setText(konference.getAdresse().getVej());
+			txfInput[5].setText("" + konference.getAdresse().getNr());
+			txfInput[6].setText(konference.getAdresse().getEtage());
+			txfInput[7].setText("" + konference.getAdresse().getPostNr());
+			txfInput[8].setText(konference.getAdresse().getBy());
+			txfInput[9].setText(konference.getAdresse().getLand());
+			
+			try
+			{
+				lvwPrisgrupper.getItems().setAll(konference.getPrisgrupper());
+
+			} catch (NullPointerException ex)
+			{
+				// do nothing
+			}
+
+		} else
+		{
+
+			txfInput[0].clear();
+			txfInput[1].clear();
+			txfInput[2].clear();
+			txfInput[3].clear();
+			txfInput[4].clear();
+			txfInput[5].clear();
+			txfInput[6].clear();
+			txfInput[7].clear();
+			txfInput[8].clear();
+			txfInput[9].clear();
+		}
+	}
+
+	// -------------------------------------------------------------------------
+
+	private void cancelAction()
+	{
+		this.hide();
+	}
+
+	private void okAction()
+	{
+
+		String titel = txfInput[0].getText().trim();
+		String tema = txfInput[1].getText().trim();
+
+		LocalDate startDato = LocalDate.MIN;
+		try
+		{
+			startDato = LocalDate.parse(txfInput[2].getText().trim());
 		} catch (DateTimeParseException ex)
 		{
 			// do nothing
 		}
-    	
-    	LocalDate slutDato = LocalDate.MIN;
-    	try
+
+		LocalDate slutDato = LocalDate.MIN;
+		try
 		{
-        	slutDato = LocalDate.parse(txfInput[3].getText().trim());
+			slutDato = LocalDate.parse(txfInput[3].getText().trim());
 		} catch (DateTimeParseException ex)
 		{
 			// do nothing
 		}
 
+		String vej = txfInput[4].getText().trim();
 
-    	String vej = txfInput[4].getText().trim();
-    	
-    	int nr = -1;
-    	try
+		int nr = -1;
+		try
 		{
-        	nr = Integer.parseInt(txfInput[5].getText().trim());
-		} catch (NumberFormatException ex)
-		{
-			// do nothing
-		}
-    	
-    	String etage = txfInput[6].getText().trim();
-    	
-    	int postNr = -1;
-    	try
-		{
-        	postNr = Integer.parseInt(txfInput[7].getText().trim());
+			nr = Integer.parseInt(txfInput[5].getText().trim());
 		} catch (NumberFormatException ex)
 		{
 			// do nothing
 		}
 
-    	String by = txfInput[8].getText().trim(); 
-    	String land = txfInput[9].getText().trim();
+		String etage = txfInput[6].getText().trim();
 
-        if (titel.length() == 0) {
-            lblError.setText("Titel er tom");
-            return;
-        }
-        else if (tema.length() == 0)
+		int postNr = -1;
+		try
+		{
+			postNr = Integer.parseInt(txfInput[7].getText().trim());
+		} catch (NumberFormatException ex)
+		{
+			// do nothing
+		}
+
+		String by = txfInput[8].getText().trim();
+		String land = txfInput[9].getText().trim();
+
+		if (titel.length() == 0)
+		{
+			lblError.setText("Titel er tom");
+			return;
+		} else if (tema.length() == 0)
 		{
 			lblError.setText("Tema er tom");
 			return;
-		}
-        else if (startDato == LocalDate.MIN)
- 		{
- 			lblError.setText("Start Dato er tom");
- 			return;
- 		}
-        else if (slutDato == LocalDate.MIN)
+		} else if (startDato == LocalDate.MIN)
+		{
+			lblError.setText("Start Dato er tom");
+			return;
+		} else if (slutDato == LocalDate.MIN)
 		{
 			lblError.setText("Slut Dato er tom");
 			return;
-		}
-        else if (vej.length() == 0)
+		} else if (vej.length() == 0)
 		{
 			lblError.setText("Vej er tom");
 			return;
-		}
-        else if (nr <=0)
+		} else if (nr <= 0)
 		{
 			lblError.setText("Nr er ugyldigt");
 			return;
-		}
-        else if (postNr <=0)
+		} else if (postNr <= 0)
 		{
 			lblError.setText("Post Nr er ugyldigt");
 			return;
-		}
-        else if (by.length() == 0)
+		} else if (by.length() == 0)
 		{
 			lblError.setText("By er ugyldigt");
 			return;
-		}
-        else if (land.length() == 0)
+		} else if (land.length() == 0)
 		{
 			lblError.setText("Land er tom");
 			return;
 		}
-        
 
-        // Call service methods
-        if (konference != null) {
-            Service.updateMiljøkonference(konference, titel, tema, startDato, slutDato, vej, nr, etage, postNr, by, land);
-        } else {
-            Service.createMiljøkonference(titel, tema, startDato, slutDato, vej, nr, etage, postNr, by, land);
-        }
+		// Call service methods
+		if (konference != null)
+		{
+			Service.updateMiljøkonference(konference, titel, tema, startDato, slutDato, vej, nr, etage, postNr, by, land);
+		} else
+		{
+			Service.createMiljøkonference(titel, tema, startDato, slutDato, vej, nr, etage, postNr, by, land);
+		}
 
-        this.hide();
-    }
+		this.hide();
+	}
 
-    private void deletePrisgruppeAction()
-    {
-    	
-    }
-    private void createPrisgruppeAction()
-    {
- 
-    }
+	private void createPrisgruppeAction()
+	{
+		PrisgruppeWindow dia = new PrisgruppeWindow("Opret Prisgruppe", konference);
+		dia.showAndWait();
+		
+		initControls();
+	}
+
+	private void deletePrisgruppeAction()
+	{
+		Prisgruppe prisgruppe = lvwPrisgrupper.getSelectionModel().getSelectedItem();
+		
+		Stage owner = (Stage) this.getScene().getWindow();
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Fjern Prisgruppe");
+		alert.initOwner(owner);
+		alert.setHeaderText("Er du sikker?");
+
+		// Wait for the modal dialog to close
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK)
+		{
+			Service.deletePrisgruppe(prisgruppe, konference);
+			initControls();
+		}
+		
+
+	}
+
 }
